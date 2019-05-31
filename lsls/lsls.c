5b4a3a1b62fd979121ec4873facbfcdb76fd9216
Zooheck <davidscottflack@gmail.com>
2019-05-31 14:04:04 -0500

    Remove unnecessary helper function.

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -4,30 +4,8 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-long int findSize(char file_name[])
-{
-  // opening the file in read mode
-  FILE *fp = fopen(file_name, "r");
-
-  // checking if the file exist or not
-  if (fp == NULL)
-  {
-    printf("File Not Found!\n");
-    return -1;
-  }
-  fseek(fp, 0L, SEEK_END);
-  // calculating the size of the file
-  long int res = ftell(fp);
-
-  // closing the file
-  fclose(fp);
-  return res;
-}
-
 int main(int argc, char **argv)
 {
-  int i;
-
   DIR *d;
   struct dirent *dir;
   d = opendir(argv[1]);
